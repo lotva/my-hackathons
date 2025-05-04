3abb85a9e439669cdc92a2a969558ea9415a1b17
lotva <dlotva@gmail.com>
2025-05-04 19:55:50 +0300

    chore: init with `pnpm create vike@latest --vue --eslint --prettier`

---
+++ eslint.config.ts
@@ -0,0 +1,74 @@
+import eslint from "@eslint/js";
+import prettier from "eslint-plugin-prettier/recommended";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import vueParser from "vue-eslint-parser";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "dist/*",
+      // Temporary compiled files
+      "**/*.ts.build-*.mjs",
+
+      // JS files at the root of the project
+      "*.js",
+      "*.cjs",
+      "*.mjs",
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: false,
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        1,
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-namespace": 0,
+    },
+  },
+
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+  },
+
+  ...pluginVue.configs["flat/recommended"],
+
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/max-attributes-per-line": "off",
+      "vue/html-self-closing": "off",
+    },
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  prettier,
+);
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import md from "unplugin-vue-markdown/vite";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [
+    vike(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    md({}),
+  ],
+  build: {
+    target: "es2022",
+  },
+});
