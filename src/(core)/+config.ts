78a6563970dcca2112b367405851cb620087e957
lotva <dlotva@gmail.com>
2025-05-13 02:39:49 +0300

    refactor(config): remove unused fields

--- src/(core)/+config.ts
+++ src/(core)/+config.ts
@@ -7,12 +7,7 @@ import vikeVue from 'vike-vue/config'
 // https://vike.dev/config
 
 export default {
-	description: 'Demo showcasing Vike',
-
 	extends: vikeVue as typeof vikeVue,
 	// https://vike.dev/Layout
 	Layout,
-
-	// https://vike.dev/head-tags
-	title: 'My Vike App',
 } satisfies Config
