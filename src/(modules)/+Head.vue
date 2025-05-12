5d1f33c6899c6746c376f084b1a77f78cafe72c3
lotva <dlotva@gmail.com>
2025-05-13 02:49:12 +0300

    feat(head): add icons

---
+++ src/(modules)/+Head.vue
@@ -0,0 +1,40 @@
+<template>
+	<meta
+		property="og:type"
+		content="website"
+	/>
+
+	<meta
+		property="og:locale"
+		content="ru_RU"
+	/>
+
+	<meta
+		property="og:site_name"
+		content="Мои хакатоны"
+	/>
+
+	<link
+		rel="icon"
+		type="image/svg+xml"
+		sizes="any"
+		:href="iconAny"
+	/>
+
+	<link
+		rel="icon"
+		type="image/png"
+		sizes="180x180"
+		:href="icon180"
+	/>
+
+	<link
+		rel="apple-touch-icon"
+		:href="icon180"
+	/>
+</template>
+
+<script setup lang="ts">
+	import icon180 from '@/(core)/ui/icon-180.png'
+	import iconAny from '@/(core)/ui/icon-any.svg'
+</script>
