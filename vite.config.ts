7d65f0d9e220b78fdf8455aae53a82193d3b7b8c
lotva <dlotva@gmail.com>
2025-06-17 03:24:47 +0300

    feat: add support for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,44 @@ import vikeRoutegen from '@blankeos/vike-routegen'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'node:url'
 import vike from 'vike/plugin'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-	build: {
-		target: 'es2022',
-	},
+export default defineConfig(({ mode }) => {
+	const environment = loadEnv(mode, process.cwd())
+	const allowedHosts = environment.VITE_ALLOWED_HOSTS?.split(',') ?? []
 
-	plugins: [
-		vike(),
-		vikeRoutegen({
-			outputPath: 'src/(core)/route-tree.gen.ts',
-		}),
-		vue({
-			include: [/\.vue$/, /\.md$/],
-			template: {
-				compilerOptions: {
-					isCustomElement: (tag) => tag === 'search',
-				},
-			},
-		}),
-	],
+	if (!environment.VITE_ALLOWED_HOSTS) {
+		throw new Error('VITE_ALLOWED_HOSTS is required in .env')
+	}
 
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('src', import.meta.url)),
+	return {
+		build: {
+			target: 'es2022',
 		},
-	},
+
+		plugins: [
+			vike(),
+			vikeRoutegen({
+				outputPath: 'src/(core)/route-tree.gen.ts',
+			}),
+			vue({
+				include: [/\.vue$/, /\.md$/],
+				template: {
+					compilerOptions: {
+						isCustomElement: (tag) => tag === 'search',
+					},
+				},
+			}),
+		],
+
+		preview: {
+			allowedHosts,
+		},
+
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('src', import.meta.url)),
+			},
+		},
+	}
 })
